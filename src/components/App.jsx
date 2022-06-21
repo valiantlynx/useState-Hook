1815e26f3270c822e566f3d7e49075bc61c7c0e8
valiantlynx <86688436+valiantlynx@users.noreply.github.com>
2022-06-21 11:52:05 +0100

    usestate learning

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return <div />;
+  const [count, setCount] = useState(0);
+
+  //const [red, green, blue] = [9, 132, 227];  deconstructing arrays
+
+  function increase() {
+    setCount(count + 1);
+  }
+  function decrease() {
+    setCount(count - 1);
+  }
+
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={increase}>+</button>
+      <button onClick={decrease}>-</button>
+    </div>
+  );
 }
 
 export default App;
